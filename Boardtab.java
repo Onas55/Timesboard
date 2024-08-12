5ec5b8a5d41b2b57b865b0b5bdce67b1752f9b64
Onas55 <Onas54@icloud.com>
2024-08-12 08:05:04 -0700

    Multiplication

---
+++ Boardtab.java
@@ -0,0 +1,12 @@
+public class Boardtab {
+    
+
+public static void main(String[] args) {
+    for(int w = 1; w <= 100; w++){
+        for(int i = 1; i <= 10; i++){
+            int multiplication = w * i;
+
+            System.out.println(w + "X" + i + "=" + multiplication );
+        }
+    }
+}}
